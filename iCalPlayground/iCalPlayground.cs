ac93d38f4663d2388eb7fe4c50c309585032d108
skay97 <salmankarimzindani@hotmail.com>
2023-09-13 14:32:20 -0500

    chore: creates solution and project

---
+++ iCalPlayground/iCalPlayground.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace iCalPlayground
+{
+    internal class iCalPlayground
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
